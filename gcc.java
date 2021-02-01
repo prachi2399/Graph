796d3396fbcc2811e2c38577eeb9378e23530735
prachi2399 <prachi2399@gmail.com>
2021-02-01 22:04:31 +0530

    Create gcc.java

---
+++ gcc.java
@@ -0,0 +1,54 @@
+public class gcc{
+    static class Edge {
+        int src;
+        int nbr;
+        int wt;
+
+        Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList < Edge > [] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+            graph[i] = new ArrayList < > ();
+        }
+
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+            String[] parts = br.readLine().split(" ");
+            int v1 = Integer.parseInt(parts[0]);
+            int v2 = Integer.parseInt(parts[1]);
+            int wt = Integer.parseInt(parts[2]);
+            graph[v1].add(new Edge(v1, v2, wt));
+            graph[v2].add(new Edge(v2, v1, wt));
+        }
+
+        ArrayList < ArrayList < Integer >> comps = new ArrayList < > ();
+
+        boolean[] vis=new boolean[vtces];
+         for(int v=0;v<vtces;v++){
+             if(vis[v]==false){
+             ArrayList<Integer> comp=new ArrayList<>();
+             drawTreeGeneralComp(graph,v,comp,vis);
+             comps.add(comp);}
+         }
+        System.out.println(comps);
+        //to get no. of connected components
+        System.out.println(comps.size());
+    }
+     public static void drawTreeGeneralComp(ArrayList < Edge > [] graph, int src, ArrayList<Integer> comp, boolean[] vis){
+         vis[src]=true;
+         comp.add(src);
+         for(Edge e : graph[src]){
+             if(!vis[e.nbr]){
+             drawTreeGeneralComp(graph, e.nbr, comp, vis);}
+         }
+     }
+}
