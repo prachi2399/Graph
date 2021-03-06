a81c8ae2f867a10f3d0f60d54d4aea9d624935ab
prachi2399 <prachi2399@gmail.com>
2021-03-06 23:43:08 +0530

    Update numberIsland.java

--- numberIsland.java
+++ numberIsland.java
@@ -36,4 +36,32 @@ public class numberIsland{
              }
          }
      }
+
+     //leetcode perimeter question
+     public int islandPerimeter(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0)
+        return 0;
+
+        int[][] dir={{1,0},{0,1},{0,-1},{-1,0}};
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0,nbr=0;
+         for(int i=0;i<n;i++){
+             for(int j=0;j<m;j++){
+              if(grid[i][j]==1){
+                  count++;
+               for(int d=0;d<4;d++)
+               {   
+                 int r=i+dir[d][0];
+                 int c=j+dir[d][1];
+                 if(r>=0&&c>=0&&r<grid.length&&c<grid[0].length&&grid[r][c]==1){
+                 nbr++;
+               }
+            }
+           }
+         }
+        }    
+          return  count*4-nbr;
+    }
+    
 }
