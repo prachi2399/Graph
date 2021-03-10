5365b51ae3126614585970db82b7d8b085113cd0
prachi2399 <prachi2399@gmail.com>
2021-03-10 16:15:38 +0530

    Update prims.java

--- prims.java
+++ prims.java
@@ -61,6 +61,126 @@ public class prims{
             }
         }
         }
-        
     }
+    
+    
+    public static class Edge {
+        int v = 0;
+        int w = 0;
+
+        Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int w) {
+        graph[u].add(new Edge(v, w));
+        graph[v].add(new Edge(u, w));
+    }
+
+    public static void display(int N, ArrayList<Edge>[] graph) {
+        for (int i = 0; i < N; i++) {
+            System.out.print(i + " -> ");
+            for (Edge e : graph[i]) {
+                System.out.print("(" + e.v + ", " + e.w + ") ");
+            }
+            System.out.println();
+        }
+    }
+
+    public class primsPair {
+        int vtx = 0;
+        int par = 0;
+        int wt = 0;
+
+        public primsPair(int vtx, int par, int wt) {
+            this.vtx = vtx;
+            this.par = par;
+            this.wt = wt;
+        }
+    }
+
+    public static void primsAlgo_02(int src, int N, boolean[] vis, ArrayList<Edge>[] graph){
+        ArrayList<Edge>[] MST=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            graph[i]=new ArrayList<>();
+        }
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
+            return a.wt-b.wt;
+        });
+
+        pq.add(new primsPair(src,-1,0));
+        int NumberOfEdges=0;
+        // while (que.size() != 0) { // for disconnected graph and more generic way
+        // while (que.size() != 0 && NumberOfEdges < N - 1) { // for disconnected graph
+            while (NumberOfEdges < N - 1) { // when you know graph is connected.
+                primsPair p = que.remove();
+
+                if(vis[p.vtx]) continue;
+
+                if(p.par!=-1){
+                    addEdge(MST,p.vtx,p.par,p.wt);
+                    NumberOfEdges++;
+                }
+
+                vis[p.vtx]=true;
+                for(Edge e:graph[p.vtx]){
+                    if(!vis[e.vtx]){
+                        pq.add(new primsPair(e.vtx,p.vtx,e.wt));
+                    }
+                }
+            }
+        }
+
+    
+        public static void primsAlgo_02(int src, int N, boolean[] vis, int[] dis, int[] par, ArrayList<Edge>[] graph){
+            ArrayList<Edge>[] MST=new ArrayList<>();
+            for(int i=0;i<N;i++){
+                graph[i]=new ArrayList<>();
+            }
+    
+            PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->{
+                return a.wt-b.wt;
+            });
+    
+            pq.add(new primsPair(src,-1,0));
+            int NumberOfEdges=0;
+            // while (que.size() != 0) { // for disconnected graph and more generic way
+            // while (que.size() != 0 && NumberOfEdges < N - 1) { // for disconnected graph
+                while (NumberOfEdges < N - 1) { // when you know graph is connected.
+                    primsPair p = que.remove();
+    
+                    if(vis[p.vtx]) continue;
+    
+                    if(p.par!=-1){
+                        addEdge(MST,p.vtx,p.par,p.wt);
+                        NumberOfEdges++;
+                    }
+    
+                    vis[p.vtx]=true;
+                    for(Edge e:graph[p.vtx]){
+                        if(!vis[e.vtx]&&e.w<dis[e.vtx]){
+                            dis[e.vtx]=e.w;
+                            par[e.vtx]=p.vtx;
+                            pq.add(new primsPair(e.vtx,p.vtx,e.wt));
+                        }
+                    }
+                }
+
+                public static void prims(){
+                    int[] dis=new int[N];
+                    int[] par=new int[N];
+                    boolean[] vis=new boolean[N];
+                    Arrays.fill(dis,(int)1e8);
+                    Arrays.fill(par,-1);
+
+                    for(int i=0;i<N;i++){
+                        if(!vis[i]){
+                            primsAlgo_02(i,N,graph);
+                        }
+                    }
+                }
+            }    
 }
